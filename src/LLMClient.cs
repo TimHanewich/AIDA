029b68a2920dc6ccea2764ce666d7a69527067fb
Tim Hanewich <tahanewich@live.com>
2025-02-17 16:06:16 -0500

    Base functionality

--- src/LLMClient.cs
+++ src/LLMClient.cs
@@ -1,11 +1,50 @@
 using System;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace AIDA
 {
     public class LLMClient
     {
-        
+        public static async Task<JObject> CallAsync(JArray messages, JArray tools)
+        {
+            HttpRequestMessage req = new HttpRequestMessage();
+            req.Method = HttpMethod.Post;
+            req.RequestUri = new Uri("http://localhost:11434/api/chat");
+
+            //Construct body
+            JObject body = new JObject();
+            body.Add("model", "qwen2.5:0.5b");
+            body.Add("stream", false);
+            body.Add("messages", messages);
+            if (tools.Count > 0)
+            {
+                body.Add("tools", tools);
+            }
+
+            //Add body to request
+            req.Content = new StringContent(body.ToString(), Encoding.UTF8, "application/json");
+            
+            //Make HTTP calls
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage resp = await hc.SendAsync(req);
+            string content = await resp.Content.ReadAsStringAsync();
+            if (resp.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("Call to model failed with code '" + resp.StatusCode.ToString() + "'. Msg: " + content);
+            }
+            
+            //Strip out message portion
+            JObject contentjo = JObject.Parse(content);
+            JProperty? prop_message = contentjo.Property("message");
+            if (prop_message == null)
+            {
+                throw new Exception("Property 'message' not in model's response.");
+            }
+            JObject ToReturn = (JObject)prop_message.Value;
+            return ToReturn;
+        }
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AIDA
 {
@@ -6,7 +8,61 @@ namespace AIDA
     {
         public static void Main(string[] args)
         {
+            Chat().Wait();
+        }
+
+        public static async Task Chat()
+        {
             Console.WriteLine("Hello!");
+
+            JArray messages = new JArray();
+            JArray tools = new JArray();
+
+            while (true)
+            {
+                //Input from user
+                string? input = null;
+                while (input == null)
+                {
+                    Console.Write("> ");
+                    input = Console.ReadLine();
+                }
+                
+                //Construct request
+                JObject NewMsg = new JObject();
+                NewMsg.Add("role", "user");
+                NewMsg.Add("content", input);
+
+                //Send to model
+                Console.Write("Thinking...");
+                messages.Add(NewMsg);
+                JObject ModelResponse = await LLMClient.CallAsync(messages, tools);
+                Console.WriteLine();
+
+                //Is the response a tool call?
+                if (ModelResponse.Property("tool_calls") != null) //there was a tool call!
+                {
+
+                }
+                else //it is noraml content
+                {
+                    
+                    //Parse out and print the response
+                    JProperty? content = ModelResponse.Property("content");
+                    if (content != null)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(content.Value.ToString());
+                        Console.WriteLine();
+                    }
+
+                    //Add the model's response to the messages
+                    messages.Append(ModelResponse);
+                    
+                }
+  
+                
+            }
         }
     }
 }
