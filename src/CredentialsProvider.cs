28354bdc329baac8b192748a0ceb7378791f6149
Tim Hanewich <tahanewich@live.com>
2025-02-21 14:08:31 -0500

    Removed credentials

--- src/CredentialsProvider.cs
+++ src/CredentialsProvider.cs
@@ -4,7 +4,7 @@ namespace AIDA
 {
     public static class CredentialsProvider
     {
-        public static string TargetUri = "https://ai-testaistudio030597089470.openai.azure.com/openai/deployments/timh-gpt4o-mini/chat/completions?api-version=2024-08-01-preview";
-        public static string ApiKey = "52799f2a98ac497baddc5da03e6c3cdd";
+        public static string TargetUri = "";
+        public static string ApiKey = "";
     }
 }
